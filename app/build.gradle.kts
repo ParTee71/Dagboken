78b530a27bea4c278ee89c2c0e3c6b3a9bbd930a
Mats Nilsson <partee71@gmail.com>
2026-06-21 18:00:02 +0200

    Release v2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "se.partee71.dagboken"
         minSdk = 30
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.1.0"
+        versionCode = 3
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
